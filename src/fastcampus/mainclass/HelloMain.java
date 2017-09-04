74438ab7c9bae2cd96567fae85b8cb8bf0b35284
RicheyHans <ifyouteolerate@gmail.com>
2017-09-04 17:40:11 +0900

    170904 lecture finished

--- src/fastcampus/mainclass/HelloMain.java
+++ src/fastcampus/mainclass/HelloMain.java
@@ -4,7 +4,14 @@ public class HelloMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		System.out.println("2+3="+(2+3));
+		System.out.println("2-3="+(2-3));
+		System.out.println("2*3="+(2*3));
+		System.out.println("2/3="+(2/3));
+		System.out.println("5%3="+(5%3));
+		System.out.println("10%3="+(10%3));
+		double dou = 5.0 % 4.2;
+		System.out.println("5.0%4.2="+dou);
 	}
 
 }
--- src/fastcampus/mainclass/Sub.java
+++ src/fastcampus/mainclass/Sub.java
@@ -1,5 +1,14 @@
 package fastcampus.mainclass;
 
 public class Sub {
-
+	public String changeNumberToString(int number) {
+		//return Integer.toString(number);
+		return number+"";
+	}
+	
+	public int changeStringToInteger(String str) {
+		return Integer.parseInt(str);
+	}
+	
+	int sum = changeStringToInteger("13");
 }
